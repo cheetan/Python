791e9d5302f0ab4c44299bd342bf48f974736810
cheetan <catalinmihaip@yahoo.com>
2019-01-01 22:36:55 +0200

    CreateHDBUserStore

--- CreateHDBUserStore.py
+++ CreateHDBUserStore.py
@@ -1,29 +1,43 @@
 #
 #
 # This script is intended to create the keys in hdbuserstore for HANA Non-MDC and HANA MDC, Single Node
+# Author: Catalin Mihai Popa -> i324220
 #
 #
-import subprocess, os, sys
+import os, subprocess, shutil, datetime
+from datetime import datetime
 
 
-# from datetime import datetime
-
-class HdbUserStoreClass:
-
-    def getparameters(self):
-        output = subprocess.check_output('HDB info', shell=True).splitlines(1)
-        print(output)
-
-    def createhdbuserstorehanamdc(self):
+class HdbUserStoreClass():
+    def createHdbUserStoreForHANAMDC(self):
         print()
 
-    def createhdbuserstorehananonmdc(self):
+    def createHdbUserStoreForHANANonMDC(self):
         print()
 
+    def checkifHANA_NonMDC_MDC(self):
+
+        output = subprocess.check_output(['hdbnsutil -printSystemInformation'], shell=True).splitlines(1)
+
+        for line in output:
+            if "SingleDB" in line:
+                self.is_mdc = False
+                dateexecution = datetime.now()
+                print(dateexecution.strftime("Date and time when the script was executed: %x, %H:%M"))
+                print(self.is_mdc)
+
+            else:
+                if "MultiDB" in line:
+                    self.is_mdc = True
+                    dateexecution = datetime.now()
+                    print(dateexecution.strftime("Date and time when the script was executed: %x, %H:%M"))
+                    print(self.is_mdc)
 
 def main():
-    output = subprocess.check_output('HDB info', shell=True).splitlines(1)
-    subprocess.call(["echo","hello"])
-    subprocess.call(['echo',output])
-    if __name__ == "__main__":
-        main()
+    # process = subprocess.Popen(['hdbnsutil', '-printSystemInformation'], stdout=subprocess.PIPE)
+    # outCheckHana_NonMDC_MDC = process.communicate()[0]
+    husc = HdbUserStoreClass()
+    husc.checkifHANA_NonMDC_MDC()
+
+if __name__ == '__main__':
+    main()
